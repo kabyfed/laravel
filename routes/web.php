2b6cfbc54e2da1ea047252f77077ca7cdd5a613d
Artem <artem0fedoseev15@mail.ru>
2024-02-07 19:29:58 +0300

    laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,44 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/',function(){
     return view('welcome');
 });
+
+//Задание 9.1
+Route::get('/user/{id?}', function($id = 0){
+    return "Передан параметр $id";
+});
+
+//Задание 9.2
+Route::get('/{year}/{month}/{day}', function($year, $month, $day){
+    $date = $year . '-' . $month . '-' . $day;
+    echo "Дата: " . $date.'<br>';
+
+    $days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    return "День недели: " . $days[date("w", strtotime($date))];
+})->where([
+    'year' => '(19|20)\d{2}$',
+    'month' => '^(0?[1-9]|1[012])$',
+    'day' => '^[0-9]*$'
+]);
+
+//Задание 9.3
+Route::get('names/{name}',function($name){
+    $users = [ 
+        'user1' => 'city1', 
+        'user2' => 'city2', 
+        'user3' => 'city3', 
+        'user4' => 'city4', 
+        'user5' => 'city5'
+       ];
+    if (array_key_exists($name,$users)) {
+        return $users[$name];
+    } else{
+        return 'Вы ввели неверное имя';
+    }
+});
+
+//Задание 9.4
+Route::get('/pages/show/{id}', [PageController::class, 'showOne']);
+Route::get('/page/all', [PageController::class, 'showAll']);
